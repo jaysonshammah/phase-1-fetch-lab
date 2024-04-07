63f256a036b552bc1889e7e2245ab59ee86c76bb
jayson <jaysonshammah3@gmail.com>
2024-04-08 01:28:14 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+  
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks(books))
 }
 
 function renderBooks(books) {
@@ -11,7 +33,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
